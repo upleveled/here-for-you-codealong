c7f5a25d6cd45e951a0e184d08ece422ea2cc4d2
Karl Horky <karl.horky@gmail.com>
2021-07-28 15:36:01 +0200

    Remove unnecessary rules

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,5 +4,20 @@ module.exports = {
       url: ['http://localhost:3000/'],
       startServerCommand: 'yarn start',
     },
+    assert: {
+      assertions: {
+        'apple-touch-icon': 'off',
+        'html-has-lang': 'off',
+        'installable-manifest': 'off',
+        'maskable-icon': 'off',
+        'offline-start-url': 'off',
+        'service-worker': 'off',
+        'splash-screen': 'off',
+        'themed-omnibox': 'off',
+        'works-offline': 'off',
+        'unsized-images': 'off',
+        'legacy-javascript': 'off',
+      },
+    },
   },
 };
