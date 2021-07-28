0879eb92db68b798d1ec7be3239c918d8c916648
Karl Horky <karl.horky@gmail.com>
2021-07-28 15:41:23 +0200

    Add image

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
@@ -64,6 +64,7 @@ export default function Home() {
           </span>
         </a>
       </footer>
+      <img src="chipmunk.jpg" />
     </div>
-  )
+  );
 }
